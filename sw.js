d17e14898ec300fa5f768c4e81ecde014d8a7a3f
rsalata <58432124+rsalata@users.noreply.github.com>
2025-10-20 18:42:37 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-
 const CACHE = 'workout-demo-v2';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./sw.js','./icons/icon-192.png','./icons/icon-512.png'];
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./sw.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
